e27b6452b840afc0b76b886db3357b7403c93855
Carlos Silveira <silveira.alberto24@gmail.com>
2019-02-26 05:31:04 -0700

    Se creo la carpeta layouts y agrego el nav

---
+++ resources/views/front/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+Todos los derechos reservados <i class="fas fa-anchor"></i>
+</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('front.layouts.plantilla',['titulo'=>'Bienvenido: Inicio'])
+@section('container')
+<!-- <div class="">
+  <h1>Working</h1>
+</div> -->
+@endsection
