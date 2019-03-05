ac73e444710f38326a5b09a03031e6e943f975bd
Carlos Silveira <silveira.alberto24@gmail.com>
2019-03-05 13:14:38 -0700

    Prueba dashboard

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{ asset('/css/all.min.css')}}">
     <link rel="stylesheet" href="{{ asset('/css/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/login.css')}}">
     <link rel="stylesheet" href="{{ asset('/css/jquery.gridder.min.css')}}">
 
   </head>
