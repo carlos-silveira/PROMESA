564c61c051353bdffb4b528fec5d795493c289d7
Carlos Silveira <silveira.alberto24@gmail.com>
2019-03-10 14:52:39 -0700

    Reorden plantilla dashboard

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,87 +1,97 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('/css/styles.css')}}">
-    <link rel="stylesheet" href="{{ asset('/css/login.css')}}">
-    <link rel="stylesheet" href="{{ asset('/css/jquery.gridder.min.css')}}">
+<html lang="en">
 
-  </head>
-  <body>
+<head>
 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <section class="login-block">
-        <div class="container">
-    	<div class="row">
-    		<div class="col-md-4 login-sec">
-    		    <h2 class="text-center">Iniciar sesion</h2>
-    		    <form class="login-form">
-      <div class="form-group">
-        <label for="exampleInputEmail1" class="text-uppercase">Usuario</label>
-        <input type="text" class="form-control" placeholder="">
+  <title>SB Admin 2 - Login</title>
 
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1" class="text-uppercase">contraseña</label>
-        <input type="password" class="form-control" placeholder="">
-      </div>
-        <div class="form-check">
-        <label class="form-check-label">
-          <input type="checkbox" class="form-check-input">
-          <small>Recordar usuario</small>
-        </label>
-        <button type="submit" class="btn btn-login float-right">Iniciar</button>
-      </div>
-    </form>
-    <div class="copy-textf"></div>
-    		</div>
-    		<div class="col-md-8 banner-sec">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                     <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                      </ol>
-                <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
-          <div class="carousel-caption d-none d-md-block">
-            <div class="banner-text">
-                <h2>This is Heaven</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-            </div>
-      </div>
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
-          <div class="carousel-caption d-none d-md-block">
-            <div class="banner-text">
-                <h2>This is Heaven</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-            </div>
-        </div>
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
-          <div class="carousel-caption d-none d-md-block">
-            <div class="banner-text">
-                <h2>This is Heaven</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-            </div>
-        </div>
-      </div>
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                  <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </a>
+                    <hr>
+                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a>
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Create an Account!</a>
+                  </div>
                 </div>
-    		</div>
-    	</div>
-    </div>
-    </section>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <script src="{{asset('/js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{asset('/js/popper.min.js')}}"></script>
-    <script src="{{asset('/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('/js/jquery.gridder.min.js')}}"></script>
-  </body>
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('/dashboard/index');
 });
 Route::get('/login', function () {
     return view('login');
