b8b79b4ee077909d273d2d56a9441c75edb2630b
chamano <chalandro7@gmail.com>
2019-04-10 10:33:46 -0800

    CONTROLLERS GG SURRENDER MINUTO 5

--- app/Http/Controllers/Portal/MateriasController.php
+++ app/Http/Controllers/Portal/MateriasController.php
@@ -21,14 +21,10 @@ class MateriasController extends Controller
   public function store(Request $request){
         $validator=Validator::make($request->all(),[
 
-          'nombre'=>'required|max:255',
-          'apellidos'=>'required|max:255',
-          'direccion'=>'required|max:255',
-          'imagen'=>'required|file|max:1024',
-          'fecha_de_nacimiento'=>'required|max:255',
-          'sexo'=>'required|max:255',
-          'tutor'=>'required|max:255',
-          'codigo_de_curso'=>'required|max:255',
+          'codigo_materia'=>'required|max:255',
+          'materia'=>'required|max:255',
+          'codigo_curso'=>'required|max:255',
+          'codigo_maestro'=>'required|max:255',
 
         ]);
         if($validator->fails()){
@@ -42,14 +38,10 @@ class MateriasController extends Controller
         $last_id= intval(Alumno::OrderBy('id','desc')->first()->id);
 
         $alu=Alumno::create([
-          'n_matricula'=>'19CNC'.($last_id+1),
-          'nombre'=>$request->nombre,
-          'apellidos'=>$request->apellidos,
-          'img'=>$image_name,
-          'fecha_de_nacimiento'=>$request->fecha_de_nacimiento,
-          'sexo'=>$request->sexo,
-          'tutor'=>$request->tutor,
-          'codigo_de_curso'=>$request->codigo_de_curso
+          'codigo_materia'=>'19CNC'.($last_id+1),
+          'materia'=>$request->materia,
+          'codigo_curso'=>$request->codigo_curso,
+          'codigo_maestro'=>$request->codigo_maestro,
         ]);
         return back()
         ->with('Listo','Se ha insertado correctamente');
